3a795a6d631b62f47454e99f3fda31baab8b2ea6
Hemant Prajapati <hemantprajapati20500@gmail.com>
2022-05-23 20:15:13 +0530

    first react app

---
+++ src/components/mobile.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Mobile(){
+    return(
+        <h1>Moblie Operating System</h1>
+        
+
+    )
+}
+function Mobliemanufactures(){
+    return (
+        <h1>Moblie Manufactures</h1>
+    )
+}
+export{ Mobile,Mobliemanufactures}
+
+
---
+++ src/components/name.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Name(){
+return (
+ <>
+ <li>Android</li>
+ <li>Black Berry</li>
+ <li>Iphone</li>
+ <li>Windown Moblie</li>
+ </>
+  
+)
+}
+
+
+function CompanyNames(){
+    return(
+    <>
+ <li>Samsung</li>
+ <li>HTC</li>
+ <li>Micromax</li>
+ <li>Apple</li>
+    </>
+    )
+}
+
+
+
+export {Name,CompanyNames};
